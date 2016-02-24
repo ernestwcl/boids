9e95d24eebd2b8f77d03abeff6022eb9ed5154df
Ernest Lo <rmapelo@ucl.ac.uk>
2016-02-24 17:23:54 +0000

    Changed rangelimit name to NBoids

--- boids.py
+++ boids.py
@@ -9,12 +9,12 @@ import random
 
 # Deliberately terrible code for teaching purposes
 
-RangeLimit = 50
+NBoids = 50
 
-boids_x=[random.uniform(-450,50.0) for x in range(RangeLimit)]
-boids_y=[random.uniform(300.0,600.0) for x in range(RangeLimit)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(RangeLimit)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(RangeLimit)]
+boids_x=[random.uniform(-450,50.0) for x in range(NBoids)]
+boids_y=[random.uniform(300.0,600.0) for x in range(NBoids)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(NBoids)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(NBoids)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
