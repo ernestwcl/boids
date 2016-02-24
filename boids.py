89960f96172b20d158b15ee4ea520d62739be6ec
Ernest Lo <rmapelo@ucl.ac.uk>
2016-02-24 17:27:13 +0000

    Replaced len(xs) with NBoids = len(xs)

--- boids.py
+++ boids.py
@@ -20,26 +20,27 @@ boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 def update_boids(boids):
 	xs,ys,xvs,yvs=boids
 	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+	Nboids = len(xs)
+	for i in range(Nboids):
+		for j in range(Nboids):
+			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/Nboids
+	for i in range(Nboids):
+		for j in range(Nboids):
+			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/Nboids
 	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
+	for i in range(Nboids):
+		for j in range(Nboids):
 			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
 				xvs[i]=xvs[i]+(xs[i]-xs[j])
 				yvs[i]=yvs[i]+(ys[i]-ys[j])
 	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
+	for i in range(Nboids):
+		for j in range(Nboids):
 			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/Nboids
+				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/Nboids
 	# Move according to velocities
-	for i in range(len(xs)):
+	for i in range(Nboids):
 		xs[i]=xs[i]+xvs[i]
 		ys[i]=ys[i]+yvs[i]
 
