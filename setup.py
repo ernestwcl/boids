b1d7c9fd9ad79aea016ace0eefc38556de69ed7a
Ernest Lo <rmapelo@ucl.ac.uk>
2016-02-27 18:43:53 +0000

    changed setup file

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Boids",
-    version = "0.1",
+    version = "1.0",
     packages = find_packages(exclude=['*test']),
+    description = "Boid flocking simulation",
+    author = "Ernest Lo",
+    author_email = "ernest.lo.15@ucl.ac.uk",
+    url = "https://github.com/ernestwcl/boids",
     scripts = ['Boids/command.py'],
     install_requires = ['argparse','numpy','matplotlib']
 )
