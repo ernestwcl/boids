c1ca76975f0edae841666e8cb92cb2604f29a470
Ernest Lo <rmapelo@ucl.ac.uk>
2016-02-29 12:47:58 +0000

    added more comments

--- Boids/boids.py
+++ Boids/boids.py
@@ -11,32 +11,38 @@ class BoidFlock(object):
 
 	def __init__(self, config = 0): 
 
-		#self.number = 50
-		#self.lowlim = lowerposlimit = np.array([-450.0,300.0])
-		#self.uplim = upperposlimit = np.array([50.0,600.0])
-		#self.lowvlim = lowervellimit = np.array([0,-20.0])
-		#self.upvlim = uppervellimit = np.array([10.0,20.0])
 		if config == 0:
-		
+		#if no config file, default values
 			self.number = 50
 			self.lowlim = lowerposlimit = np.array([-450.0,300.0])
 			self.uplim = upperposlimit = np.array([50.0,600.0])
 			self.lowvlim = lowervellimit = np.array([0,-20.0])
 			self.upvlim = uppervellimit = np.array([10.0,20.0])
+			self.tooclose = 100
+			self.middlestrength  = 0.01
+			self.flocksize = 10000
+			self.matchweight = 0.125
 		
 		else:
+		#load config file values
 			self.config = yaml.load(open(config))
 			self.number = self.config['NUMBER_OF_BOIDS']
 			self.lowlim  = np.array(self.config['LOWER_POS_LIM'])
 			self.uplim = np.array(self.config['UPPER_POS_LIM'])
 			self.lowvlim = np.array(self.config['LOWER_VEL_LIM'])
 			self.upvlim = np.array(self.config['UPPER_VEL_LIM'])
+			self.tooclose = np.array(self.config['PROXIMITY'])
+			self.middlestrength  = np.array(self.config['MOVEMENT_STRENGTH'])
+			self.flocksize = np.array(self.config['FLOCK_SIZE'])
+			self.matchweight = np.array(self.config['FLOCK_WEIGHT'])
 
+		#the two key attributes of this class, position and velocity, initialised here.
 		self.position = self.new_flock_positions()
 		self.velocity = self.new_flock_velocities()
 		
 
 	def new_flock_positions(self):
+		#initialises flock positions given upper and lower limits
 		NBoids = self.number
 		upper_limits = self.uplim
 		lower_limits = self.lowlim
@@ -45,9 +51,10 @@ class BoidFlock(object):
 		boidpositions  = lower_limits[:,np.newaxis] + np.random.rand(2, NBoids)*range[:,np.newaxis]
 		
 		return boidpositions
-		#self.position = boidpositions
+
 		
 	def new_flock_velocities(self):
+		#initialises flock velocities given upper and lower limits
 		NBoids = self.number
 		upper_limits = self.upvlim
 		lower_limits = self.lowvlim
@@ -56,33 +63,36 @@ class BoidFlock(object):
 		boidvelocities = lower_limits[:,np.newaxis] + np.random.rand(2, NBoids)*range[:,np.newaxis]
 
 		return boidvelocities
-		#self.velocity = boidvelocities
+
 		
 	def set_position(self,position):
+		#setter function for positions, used mainly for testing
 		self.position = position
 		
 	def set_velocity(self,velocity):
+		#setter function for velocities, used mainly for testing
 		self.velocity = velocity
 
 		
 	def update_positions(self):
+		#updates position by adding the new velocity values
 		self.position = self.position + self.velocity
 		
 		
 	def fly_to_middle(self):
-
+		#fly towards middle
 		pos1 = self.position
 		velo1 = self.velocity
 		
 		flockcenter = np.mean(pos1,1)
 		direction_to_center = pos1 - flockcenter[:,np.newaxis]
-		strength = 0.01
+		strength = self.middlestrength
 		velo1 -= (direction_to_center*strength)
 
 		self.velocity=velo1
 		
 	def avoid_collisions(self):
-
+		#fly away to avoid collisions
 		pos1 = self.position
 		velo1 = self.velocity
 
@@ -90,7 +100,7 @@ class BoidFlock(object):
 		separations = pos1[:,np.newaxis,:] - pos1[:,:,np.newaxis]
 		squared_diff = np.power(separations,2)
 		squared_dist = np.sum(squared_diff,0)
-		proximity_condition = 100
+		proximity_condition = self.tooclose
 		not_too_close = squared_dist>=proximity_condition
 		separations_if_too_close = np.copy(separations)
 		separations_if_too_close[0,:,:][not_too_close]=0
@@ -105,13 +115,15 @@ class BoidFlock(object):
 		xpos,ypos = self.position
 		xvel,yvel = self.velocity
 		# Try to match speed with nearby boids
+		flocksize = self.flocksize
+		match_speed_strength  = (self.matchweight)/(self.number)
 		for i in range(len(xpos)):
 			for j in range(len(xpos)):
-				if (xpos[j]-xpos[i])**2 + (ypos[j]-ypos[i])**2 < 10000:
+				if (xpos[j]-xpos[i])**2 + (ypos[j]-ypos[i])**2 < flocksize:
 
 
-					xvel[i]=xvel[i]+(xvel[j]-xvel[i])*0.125/50
-					yvel[i]=yvel[i]+(yvel[j]-yvel[i])*0.125/50
+					xvel[i]=xvel[i]+(xvel[j]-xvel[i])*match_speed_strength
+					yvel[i]=yvel[i]+(yvel[j]-yvel[i])*match_speed_strength
 
 		
 		
@@ -129,8 +141,8 @@ class BoidFlock(object):
 		squared_dist = np.sum(squared_diff,0)
 		
 		velocityseparation = velo1[:,np.newaxis,:] - velo1[:,:,np.newaxis]
-		flock_size = 10000
-		flock_formation_strength = 0.125
+		flock_size = self.flocksize
+		flock_formation_strength = self.matchweight
 		outside_flock = squared_dist > flock_size
 		
 		velocityseparation_if_close = np.copy(velocityseparation)
@@ -156,9 +168,9 @@ class BoidFlock(object):
 
 	def update_boids(self):
 
-
+		#update velocities
 		self.update_velocities()
-
+		#update positions
 		self.update_positions()
 
 		
